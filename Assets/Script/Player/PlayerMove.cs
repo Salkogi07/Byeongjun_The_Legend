67aa3f91406ddb477500da60cba5daa8f930488a
kogisal <kogisal099@gmail.com>
2024-11-29 14:30:46 +0900

    Player_move

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -79,11 +79,11 @@ public class PlayerMove : MonoBehaviour
     {
         moveInput = 0;
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             moveInput = -1;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             moveInput = 1;
         }
